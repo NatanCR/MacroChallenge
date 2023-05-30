4bdcbdc38798d21ca06a21b2fa689e3a2889904a
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-05-30 16:48:36 -0300

    initial commit main

---
+++ MacroChallenge/MacroChallenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MacroChallenge
+//
+//  Created by Natan de Camargo Rodrigues on 30/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacroChallenge/MacroChallenge/MacroChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroChallengeApp.swift
+//  MacroChallenge
+//
+//  Created by Natan de Camargo Rodrigues on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
