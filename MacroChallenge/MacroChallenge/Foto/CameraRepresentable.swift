20755625b76019f2e7d2228bdeefdcb3311b72ba
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2023-05-30 17:09:32 -0300

    feat - foto todo

---
+++ MacroChallenge/MacroChallenge/Foto/CameraRepresentable.swift
@@ -0,0 +1,53 @@
+//
+//  CameraRepresentable.swift
+//  MacroChallenge01_BigIdeas
+//
+//  Created by Rodrigo Ferreira Pereira on 25/05/23.
+//
+
+import AVFoundation
+import SwiftUI
+
+struct CameraRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIImagePickerController
+
+    @Binding var capturedImages: [UIImage]
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = context.coordinator // Definindo o delegate
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    // Definindo o coordinator
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CameraRepresentable
+
+        init(parent: CameraRepresentable) {
+            self.parent = parent
+        }
+
+        // Método chamado quando a imagem é capturada
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.capturedImages.append(image) // Adicionando a imagem capturada ao array capturedImages
+            }
+
+            picker.dismiss(animated: true) // Fechando a câmera
+        }
+
+        // Método chamado quando o usuário cancela a captura
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true) // Fechando a câmera
+        }
+    }
+}
