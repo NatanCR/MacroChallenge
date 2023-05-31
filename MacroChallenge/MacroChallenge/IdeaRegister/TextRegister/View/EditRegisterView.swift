b3766aac3a9ccd7c84c2059313221e0fd35b4ecf
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-05-31 17:18:46 -0300

    atualizando a ideia no mesmo formato do registro

--- MacroChallenge/MacroChallenge/IdeaRegister/TextRegister/View/EditRegisterView.swift
+++ MacroChallenge/MacroChallenge/IdeaRegister/TextRegister/View/EditRegisterView.swift
@@ -14,7 +14,6 @@ struct EditRegisterView: View {
     @ObservedObject var modelData: ModelData
     @State private var isAlertActive: Bool = false
     @EnvironmentObject var appState: AppState
-//    @State private var completeText: String
     
     var body: some View {
         GeometryReader { geo in
@@ -31,6 +30,15 @@ struct EditRegisterView: View {
                 Button("Save") {
                     //verifica ID atual e localiza no array
                     if let index = modelData.model.firstIndex(where: { $0.id == modelText.id }) {
+                        
+                        if let range = modelText.textComplete.rangeOfCharacter(from: .newlines) {
+                            let index = modelText.textComplete.distance(from: modelText.textComplete.startIndex, to: range.lowerBound)
+                            let titleIndex = modelText.textComplete.index(modelText.textComplete.startIndex, offsetBy: index)
+                            modelText.title = String(modelText.textComplete[..<titleIndex])
+                        }
+                        
+                        modelText.text = modelText.textComplete.replacingOccurrences(of: modelText.title, with: ("".trimmingCharacters(in: .whitespacesAndNewlines)))
+                        modelText = ModelText(title: modelText.title, creationDate: modelText.creationDate, modifiedDate: Date(), text: modelText.text, textComplete: modelText.textComplete)
                         //atualiza o dado na posição no indice
                         modelData.model[index] = modelText
                         //salva novamente
