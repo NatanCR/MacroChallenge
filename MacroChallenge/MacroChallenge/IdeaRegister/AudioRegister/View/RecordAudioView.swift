36ca19c8a9b7aca6a85069cbcc254708595293ee
Henrique Assis <henriquebatista061@gmail.com>
2023-06-29 00:35:07 -0300

    fix - remoção de comentário

--- MacroChallenge/MacroChallenge/IdeaRegister/AudioRegister/View/RecordAudioView.swift
+++ MacroChallenge/MacroChallenge/IdeaRegister/AudioRegister/View/RecordAudioView.swift
@@ -238,28 +238,3 @@ struct RecordAudioView_Previews: PreviewProvider {
         RecordAudioView()
     }
 }
-
-//extension LocalizedStringKey {
-//    var stringKey: String? {
-//        Mirror(reflecting: self).children.first(where: { $0.label == "key" })?.value as? String
-//    }
-//}
-//
-//extension String {
-//    static func localizedString(for key: String,
-//                                locale: Locale = .current) -> String {
-//
-//        let language = locale.languageCode
-//        let path = Bundle.main.path(forResource: language, ofType: "lproj")!
-//        let bundle = Bundle(path: path)!
-//        let localizedString = NSLocalizedString(key, bundle: bundle, comment: "")
-//
-//        return localizedString
-//    }
-//}
-//
-//extension LocalizedStringKey {
-//    func stringValue(locale: Locale = .current) -> String {
-//        return .localizedString(for: self.stringKey ?? "", locale: locale)
-//    }
-//}
