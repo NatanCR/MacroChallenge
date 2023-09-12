8dff8a81e3859050801dd195b8a0a832a4396b0c
alinerss <92606880+alinerss@users.noreply.github.com>
2023-09-12 14:04:36 -0300

    ajustes de tutorial
    tutorial em dark mode, ipad, versão em inglês

--- MacroChallenge/MacroChallenge/IdeaRegister/Idea/View/TutorialView.swift
+++ MacroChallenge/MacroChallenge/IdeaRegister/Idea/View/TutorialView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct TutorialView: View {
     @Environment(\.screenSize) var screenSize
-    @Binding var tutorialPresented: Bool
-    @State var index = 1
+    @Environment(\.colorScheme) var colorScheme
+    @Environment(\.dismiss) var dismiss
+    
+    @Binding var tutorialPresented: Bool //reconhece se o tutorial ja foi apresentado
+    @State var index = 1 //index das imagens
     
     var body: some View {
         
@@ -21,10 +24,45 @@ struct TutorialView: View {
             }
                     .frame(width: screenSize.width, height: screenSize.height)
                     .background{
-                        Image("tutorial-pt\(index)")
+                        if UIDevice.current.userInterfaceIdiom == .phone {
+                            //reconhece idioma para setar a imagem
+                            if let preferredLanguage = Locale.preferredLanguages.first {
+                                if preferredLanguage.starts(with: "pt") {
+                                    // carrega imagem em português
+                                    // identifica se está em dark ou light mode
+                                    // chama a imagem de acordo com o index
+                                    Image(colorScheme == .dark ? "tutorialdark-pt\(index)" : "tutorial-pt\(index)")
+                                } else {
+                                    // carrega imagem em inglês
+                                    // identifica se está em dark ou light mode
+                                    // chama a imagem de acordo com o index
+                                    Image(colorScheme == .dark ? "tutorialdark-en\(index)" : "tutorial-en\(index)")
+                                    
+                                }
+                            }
+                        } else if UIDevice.current.userInterfaceIdiom == .pad {
+                            //reconhece idioma para setar a imagem
+                            if let preferredLanguage = Locale.preferredLanguages.first {
+                                if preferredLanguage.starts(with: "pt") {
+                                    // carrega imagem em português
+                                    // identifica se está em dark ou light mode
+                                    // chama a imagem de acordo com o index
+                                    Image(colorScheme == .dark ? "tutorialdarkipad-pt\(index)" : "tutorialipad-pt\(index)")
+                                } else {
+                                    // carrega imagem em inglês
+                                    // identifica se está em dark ou light mode
+                                    // chama a imagem de acordo com o index
+                                    Image(colorScheme == .dark ? "tutorialdarkipad-en\(index)" : "tutorialipad-en\(index)")
+                                    
+                                }
+                            }
+                        }
                     }
+            
             .toolbar{
                 ToolbarItem(placement: .navigationBarTrailing) {
+                    
+                    // opção de pular tutorial, aparece se não for a última imagem
                     if index<=4{
                         Button{
                             tutorialPresented = true
@@ -36,39 +74,56 @@ struct TutorialView: View {
                 }
                 
                 ToolbarItem(placement: .bottomBar) {
-                    HStack(spacing: 210){
-                        if index>1{
-                            Button{
-                                index = index - 1
-                            } label: {
-                                Text("Anterior")
-                            }
-                        }
+                    HStack{
                         
-                        if index<=4{
-                            Button{
-                                index = index + 1
-                            } label: {
-                                Text("Próximo")
+                        // botão de voltar para imagem anterior
+                        HStack{
+                            if index>1{
+                                Button{
+                                    index = index - 1
+                                } label: {
+                                    Text("Anterior")
+                                        .padding(.leading)
+                                }
                             }
-                            .padding(.trailing)
-                            
-                        } else {
-                            Button{
-                                tutorialPresented = true
-                                UserDefaults.standard.set(true, forKey: "hasShownTutorial")
-                            } label: {
-                                Text("OK")
-                                    .foregroundColor(.white)
-                                    .padding(.vertical, 8)
-                                    .padding(.horizontal, 10)
-                                    .background{
-                                        Capsule(style: .circular)
-                                            .fill(Color("AccentColor"))
-                                    }
-                            }
-                            .padding(.horizontal, 20)
                         }
+                        .frame(width: screenSize.width * 0.5, alignment: .leading)
+
+                        // botão de avançar para próxima imagem ou concluir tutorial
+                        HStack{
+                            if index<=4{
+                                Button{
+                                    index = index + 1
+                                } label: {
+                                    Text("Próximo")
+                                        .padding(.trailing)
+                                }
+                                
+                            } else {
+                                Button{
+                                    //na primeira vez que o tutorial for acessado, salva acesso
+                                    if !tutorialPresented{
+                                        tutorialPresented = true
+                                        UserDefaults.standard.set(true, forKey: "hasShownTutorial") //salva no user defaults para aparecer apenas no primeiro acesso
+                                    } else {
+                                        dismiss() //se estiver visualizando da tela de info, dá dismiss
+                                    }
+                                } label: {
+                                    Text("OK")
+                                        .foregroundColor(Color("backgroundColor"))
+                                        .padding(.vertical, 8)
+                                        .padding(.horizontal, 10)
+                                        .background{
+                                            Capsule(style: .circular)
+                                                .fill(Color("AccentColor"))
+                                        }
+                                        .padding(.trailing)
+                                }
+                            }
+                            
+                        }
+                        .frame(width: screenSize.width * 0.5, alignment: .trailing)
+                        
                     }
                     .frame(width: screenSize.width, alignment: .trailing)
 
